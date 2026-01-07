bf91e89d8a2847996d09e3406d608f7565d4d394
Subre Moktar <109233694+Subre-Moktar@users.noreply.github.com>
2026-01-06 19:51:21 -0600

    Changed the behavior of the kafka consumer to latest to earliest

--- Assets/Jobs/ingest_kafka_to_landing.py
+++ Assets/Jobs/ingest_kafka_to_landing.py
@@ -27,7 +27,7 @@ def consume_batch(topic: str, batch_duration_sec: int, output_path: str) -> int:
     consumer = KafkaConsumer(
         topic,
         bootstrap_servers = ["kafka:9092"],
-        auto_offset_reset = "latest",
+        auto_offset_reset = "earliest",
         enable_auto_commit = True,
         value_deserializer = lambda v: json.loads(v.decode('utf-8'))
     )
