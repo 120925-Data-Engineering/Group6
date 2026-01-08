b710b6fee18750cd233055a7ddcb0be141e2e871
Subre Moktar <109233694+Subre-Moktar@users.noreply.github.com>
2026-01-08 14:26:22 -0600

    Producers will now create from a user pool
    	- Pool of user instead of a random user

--- Assets/dags/dag_streamflow.py
+++ Assets/dags/dag_streamflow.py
@@ -7,6 +7,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.hooks.base import BaseHook
 from datetime import datetime, timedelta
 
 
@@ -15,7 +16,37 @@ default_args = {
     # TODO: Add retry logic, email alerts, etc.
 }
 
-
+# We are trying to get the kafka information from connection
+def get_kafka_details(**context):
+    """
+    We are trying to collect the kafka information that is stored in the airflow connections using basehook
+    
+    """
+    print("Collecting connection details....Please wait")
+    
+    try:
+        kafka_connection = BaseHook.get_connection("kafka_connection")
+        
+        # Our kafka information is stored in the extra box
+        if kafka_connection.extra:
+            extra = kafka_connection.extra_dejson
+            print("Kafka Configurations")
+            
+            for key, value in extra.items():
+                print(f"{key}   :   {value}")
+            
+            # Makes the configurations into a xcomm variable
+            return extra
+        
+        else:
+            print("we found no kafka configurations")
+            return {}
+    
+    except Exception as e:
+        print(f"Connection 'kafka_connection' is not found: {e}")
+        print("Please create the connection in the airflow UI")
+        return {}
+    
 
 
 with DAG(
@@ -27,6 +58,12 @@ with DAG(
 ) as dag:
     # Notify dag start
     start = EmptyOperator(task_id = 'start')
+    
+    # Collecting the kafka information from connections
+    get_config_details = PythonOperator(
+        task_id = "get_kafka_config",
+        python_callable = get_kafka_details
+    )
     
     # Creates the topic for the kafka server if it doesn't exist
     create_topics = BashOperator(
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Group6
 Raziya Ahmed and Subre Moktar
+
+When Creating the docker compose, make sure in airflow create the connection variable
