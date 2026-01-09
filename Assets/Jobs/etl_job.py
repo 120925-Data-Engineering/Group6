03ec97670187c47bd77ee8098732b45ee36fdacd
Subre Moktar <109233694+Subre-Moktar@users.noreply.github.com>
2026-01-09 11:16:44 -0600

    Added a testing file for etl job with ipynb file

--- Assets/Jobs/etl_job.py
+++ Assets/Jobs/etl_job.py
@@ -28,10 +28,12 @@ def run_etl(spark: SparkSession, input_path_topic1: str, input_path_topic2: str,
         output_path: Gold zone path (e.g., '/opt/spark-data/gold')
     """
     # TODO: Implement
-    spark = create_spark_session("ETL_job")
+    spark = spark
     
     df_topic1 = spark.read.json(input_path_topic1)
     df_topic2 = spark.read.json(input_path_topic2)
+    
+    df_topic1.show(1)
     
     
     pass
@@ -39,4 +41,6 @@ def run_etl(spark: SparkSession, input_path_topic1: str, input_path_topic2: str,
 
 if __name__ == "__main__":
     # TODO: Create SparkSession, parse args, run ETL
+    
+    run_etl(create_spark_session("ETL_job"),'./Assets/data/landing/transaction_events_1767974907.7832654.json', './Assets/data/landing/user_events_1767974827.1332347.json', "")
     pass
